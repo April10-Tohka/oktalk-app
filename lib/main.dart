aea8e0e28b65c84d4785fe7c23153f3406d8d76f
Tohka <1324419148@qq.com>
2026-04-13 23:05:40 +0800

    使用路由导航的形式来渲染App初始页面

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 import 'pages/ai_home_page.dart';
+import 'pages/login/component.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +19,14 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
         scaffoldBackgroundColor: const Color(0xFFE8F5E9),
       ),
-      home: const AiHomePage(),
+      initialRoute: "/login",
+      routes: <String, WidgetBuilder>{
+        '/login': (BuildContext context) => const Component(), // 登录页
+        '/ai_home': (BuildContext context) => const AiHomePage(), // 你的 AI 专区首页
+        // 以后继续在这里添加其他页面，例如：
+        // '/profile': (context) => const ProfilePage(),
+        // '/settings': (context) => const SettingsPage(),
+      },
     );
   }
 }
